f32d59206c2a771b1b76251f3f4014d4ddbfae4f
Cody Tucker <bpcttheteacher@gmail.com>
2025-03-27 16:39:10 -0500

    Added Sample Book Data and Book API calls

---
+++ api/bookAPI.js
@@ -0,0 +1,65 @@
+import { clientCredentials } from "../utils/client";
+
+// URL TO DATABASE FOR PROMISES/API CALLS
+const endpoint = clientCredentials.databaseURL;
+
+// Get All Books in the Database
+const getAllBooks = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// Get a Single Book by BookID firebaseKey
+const getSingleBook = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+  // Create New Book
+const createBook = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+// Update Book (Predominantly for patching in the firebaseKey into the object itself)
+  const updateBook = (payload) => new Promise((resolve, reject) => {
+    fetch(`${endpoint}/books/${payload.firebaseKey}.json`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.text())
+      .then((data) => resolve((data)))
+      .catch(reject);
+  });
+
+export {
+  getAllBooks,
+  getSingleBook,
+  createBook,
+  updateBook
+}
